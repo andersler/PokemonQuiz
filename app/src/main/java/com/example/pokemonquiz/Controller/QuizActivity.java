d945db2ec72d66d8c69bd96ddd6f3d8f04f7b41f
Adrian <loggavdenne@gmail.com>
2023-03-23 17:41:29 +0100

    Quiz cleanup

--- app/src/main/java/com/example/pokemonquiz/Controller/QuizActivity.java
+++ app/src/main/java/com/example/pokemonquiz/Controller/QuizActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -22,178 +23,160 @@ import com.example.pokemonquiz.R;
 import com.example.pokemonquiz.ViewModel.PokemonAdapter;
 import com.example.pokemonquiz.ViewModel.PokemonViewModel;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class QuizActivity extends AppCompatActivity {
 
-    public boolean choice = false;
+    public boolean hardMode = false;
     public CountDownTimer timer;
     int score = 0;
     int answered = 0;
-
-    private PokemonViewModel pokemonViewModel;
-    private List<Pokemon> questions;
-
-    private ImageView imageView;
-
-    private Button btn1;
-    private Button btn2;
-    private Button btn3;
-    private Button btnExit;
-    public ProgressBar simpleProgressBar;
-
+    private List<Pokemon> pokemonList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_activity);
 
-        pokemonViewModel = ViewModelProviders.of(this).get(PokemonViewModel.class);
+        // Fetch the questions from the database
+        PokemonViewModel pokemonViewModel = ViewModelProviders.of(this).get(PokemonViewModel.class);
         PokemonAdapter adapter = new PokemonAdapter();
 
         pokemonViewModel.getAll().observe(this, new Observer<List<Pokemon>>() {
             @Override
             public void onChanged(List<Pokemon> pokemons) {
 
-                adapter.setPokemonList(pokemons);
+                if (pokemons != null) {
+                    adapter.setPokemonList(pokemons);
+                    pokemonList = pokemons;
 
-                // Fetch questions
-                questions = pokemons;
-
-
-                //DETTE FUNKER IKKE
-                //Bundle values = getIntent().getBundleExtra("choice");
-                //choice = values.getBoolean("choice",false);
-
-
-
-                //    Pokemon p =  pokemonViewModel.getStandardPokemonList().get(0);
-
-                //Shuffle the questions to preserve randomness
-
-                System.out.println("SHUFFLED");
-
-
-
-                // Setup imageView and buttons
-                imageView = findViewById(R.id.imageView);
-
-                btn1 = findViewById(R.id.btn1);
-                btn2 = findViewById(R.id.btn2);
-                btn3 = findViewById(R.id.btn3);
-
-                displayNextQuestion();
-
-
+                    System.out.println("Fetched " + pokemonList.size() + " questions");
+                    // Start the question loop
+                    displayNextQuestion();
+                } else {
+                    System.out.println("ERROR, NO POKEMON FOUND");
+                    returnToMainMenu();
+                }
             }
-
-
         });
-
     }
 
-
     @SuppressLint("SetTextI18n")
-
     private void displayNextQuestion() {
-        simpleProgressBar=(ProgressBar) findViewById(R.id.simpleProgressBar); // initiate the progress bar
+        // Scoreboard and progress bar updating
         TextView scoreBoard = findViewById(R.id.scoreBoard);
-        TextView feedback = findViewById(R.id.feedback);
         scoreBoard.setText("Score: " + score + "/" + answered);
-        if(choice) {
+        handleProgressBar();
+
+        // Create a list of questions that's to be displayed and add the next pokemon.
+        List<Pokemon> questions = new ArrayList<>();
+        questions.add(pokemonList.get(answered));
+
+        // Add two random pokemons from the pokemon list to the question list.
+        addRandomQuestions(2, questions);
+
+        // Set the image to the first index of the questions array
+        ImageView imageView = findViewById(R.id.imageView);
+        imageView.setImageBitmap(questions.get(0).getImage());
+
+        // Setup the correct and wrong answer buttons.
+        setupButtons(questions);
+
+        exitButton();
+    }
+
+    private void handleProgressBar() {
+
+        ProgressBar simpleProgressBar = findViewById(R.id.simpleProgressBar);
+
+        if(hardMode) {
             simpleProgressBar.setMax(10);
-
             timer = new CountDownTimer(10000, 1000) {
-
                 public void onTick(long millisUntilFinished) {
                     simpleProgressBar.setProgress((int) (millisUntilFinished / 1000));
                 }
-
                 public void onFinish() {
-                    answered++;
-                    if (answered == questions.size()) {
-                        returnToMainMenu();
-                    } else {
-                        displayNextQuestion();
-                    }
+                    returnToMainMenu();
                 }
             }.start();
+        } else { // Easy mode: Progress displays % of correct answers
+            simpleProgressBar.setMax(pokemonList.size());
+            simpleProgressBar.setProgress(score);
         }
-        else{
-            simpleProgressBar.setMax(questions.size());
-        }
-        simpleProgressBar.setProgress(score);
-        // Set the imageView to be the first element in the question array
-        imageView.setImageBitmap(questions.get(answered).getImage());
+    }
 
-        // The correct answer
-        String correctAnswer = questions.get(answered).getName();
-        System.out.println("Question order: 1: " + questions.get(0).getName() + ", 2: " + questions.get(1).getName() + ", 3:" + questions.get(2).getName());
-        System.out.println("Current question: (" + (answered+1) + ") " + questions.get(answered).getName());
-        System.out.println("Current answer: " + correctAnswer);
+    /** Adds random questions from the pokemonList to the questions list that is not already present
+     * @param n Amounts of questions to be added
+     * @param questions The pokemonList the questions should be added to
+     */
+    private void addRandomQuestions(int n, List<Pokemon> questions) {
 
-        // Get two wrong answers at random
-        ArrayList<String> wrongAnswers = new ArrayList<>();
+        Random random = new Random();
 
-        ArrayList<Pokemon> shuffled = new ArrayList<>(questions);
-        Collections.shuffle(shuffled);
+        for (int i = 0; i < n;) {
 
-        for ( Pokemon p : shuffled) {
-            if (!p.getName().equals(correctAnswer)) {
-                wrongAnswers.add(p.getName());
-                if (wrongAnswers.size() == 2) break;
+            boolean valid = true;
+            int index = random.nextInt(pokemonList.size());
+
+            for (Pokemon p : questions) {
+                if (pokemonList.get(index).getName().equals(p.getName())) {
+                    valid = false;
+                    break;
+                }
+            }
+            if (valid) {
+                questions.add(pokemonList.get(index));
+                i++;
             }
         }
+    }
 
-        // Store buttons in arraylist and shuffle
+    /** Sets up three buttons and randomized their view order such that one of the button will
+     * be the correct answer and two will be the wrong answer.
+     * @param questions The list of which the buttons should be set up for
+     */
+    @SuppressLint("SetTextI18n")
+    private void setupButtons(List<Pokemon> questions) {
+        // Setup
+        Button btn1 = findViewById(R.id.btn1);
+        Button btn2 = findViewById(R.id.btn2);
+        Button btn3 = findViewById(R.id.btn3);
+        TextView feedback = findViewById(R.id.feedback);
+
+        // Store buttons in a shuffled array for randomness
         ArrayList<Button> buttons = new ArrayList<>( Arrays.asList(btn1, btn2, btn3));
         Collections.shuffle(buttons);
 
-        // Set the correct answer button
-        buttons.get(0).setText(correctAnswer);
-        buttons.get(0).setOnClickListener(view -> {
-            score++;
-            answered++;
-            if(answered==questions.size()){
-                if(choice) {
-                    timer.cancel();
-                }
-                returnToMainMenu();
-            }
-            else {
-                feedback.setText("");
-                if(choice) {
-                    timer.cancel();
-                }
-                displayNextQuestion();
-            }
-        });
+        for (int i = 0; i < buttons.size(); i++) {
+            buttons.get(i).setText(questions.get(i).getName());
+            int correctIndex = i;
 
-        // Set the wrong answer buttons
-        for (int i = 1; i <= wrongAnswers.size(); i++) {
-            buttons.get(i).setText(wrongAnswers.get(i-1));
             buttons.get(i).setOnClickListener(view -> {
                 answered++;
-                if(answered==questions.size()){
-                    if(choice) {
-                        timer.cancel();
-                    }
+                if(hardMode) {
+                    timer.cancel();
+                }
+                if(answered == pokemonList.size()) { // If no more questions, return to main menu.
                     returnToMainMenu();
                 }
-                else {
-                    feedback.setText("Right answer was: " + correctAnswer);
-                    if(choice) {
-                        timer.cancel();
+                else {                      // If more questions, display the next one.
+                    if (correctIndex == 0) { // Correct answer - if first button was pressed
+                        score++;
+                        feedback.setText("Correct!");
+                    } else {                    // Wrong answer - if one of the wrong buttons.
+                        feedback.setText("Right answer was: " + questions.get(0).getName());
                     }
                     displayNextQuestion();
                 }
             });
         }
-        exitButton();
     }
+
     public void returnToMainMenu(){
         Intent intent = new Intent(QuizActivity.this, new MainActivity().getClass());
         startActivity(intent);
